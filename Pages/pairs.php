ce81a69b2259ac0b592e4df89acf390f357d9b66
Barnesey41 <74460619+Barnesey41@users.noreply.github.com>
2023-03-04 16:51:13 +0000

    added navbar

--- Pages/pairs.php
+++ Pages/pairs.php
@@ -1,9 +1,43 @@
 <!DOCTYPE html>
+
+<html>
 <head>
-    <link rel="stylesheet" href="styles.css" type="text/css"> <!-- link to style sheet -->
+    <title>Home</title>
+    <link rel="stylesheet" href="../Style Sheets/styles.css" type="text/css"> <!-- link to style sheet -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <html>
 <body>
+
+<!-- Check if cookies are set -->
+<?php
+echo PHP_SESSION_ACTIVE;
+    if(isset($_COOKIE['username']) && isset($_COOKIE['avatar'])){
+        if (!session_status() === PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+        $_SESSION['username'] = $_COOKIE['username'];
+        $_SESSION['avatar'] = $_COOKIE['avatar'];
+        $_SESSION['registered'] = true;
+    }
+    else if(session_status() === PHP_SESSION_ACTIVE) {
+        $_SESSION['username'] = '';
+        $_SESSION['avatar'] = '';
+        $_SESSION['registered'] = false;
+    } 
+    else{
+        session_start();
+        $_SESSION['username'] = '';
+        $_SESSION['avatar'] = '';
+        $_SESSION['registered'] = false;
+    } 
+?>
+
+<html>
+<body>
+    <?php include '../Elements/navbar.php' ?>
 
 </body>
