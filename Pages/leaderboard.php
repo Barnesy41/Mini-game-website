2064f063c5d4f8f6c9c2033ec3cf42e39e6e7671
Barnesey41 <74460619+Barnesey41@users.noreply.github.com>
2023-03-12 18:33:23 +0000

    fixed navbar

--- Pages/leaderboard.php
+++ Pages/leaderboard.php
@@ -5,10 +5,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <!-- Check if cookies are set -->
     <?php
-    if(isset($_cookie['username']) && isset($_cookie['avatar'])){
-        session_start();
-        $_SESSION['username'] = $_cookie['username'];
-        $_SESSION['avatar'] = $_cookie['avatar'];
+    if(isset($_COOKIE['username']) && isset($_COOKIE['avatar'])){
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+        $_SESSION['username'] = $_COOKIE['username'];
+        $_SESSION['avatar'] = $_COOKIE['avatar'];
         $_SESSION['registered'] = true;
     }
     else if(session_status() === PHP_SESSION_ACTIVE) {
@@ -21,7 +23,7 @@
         $_SESSION['username'] = '';
         $_SESSION['avatar'] = '';
         $_SESSION['registered'] = false;
-    } 
+    }
     ?>
 </head>
 
