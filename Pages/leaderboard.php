0f97d3f1e8e2365860a548d88a8a0c5718746df2
Barnesey41 <74460619+Barnesey41@users.noreply.github.com>
2023-03-14 21:57:31 +0000

    leaderboard implemented

--- Pages/leaderboard.php
+++ Pages/leaderboard.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>
-    <link rel="stylesheet" href="../Style Sheets/styles.css" type="text/css"> <!-- link to style sheet -->
+    <link rel="stylesheet" href="../Style-Sheets/styles.css" type="text/css"> <!-- link to style sheet -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <!-- Check if cookies are set -->
@@ -25,12 +25,38 @@
         $_SESSION['registered'] = false;
     }
     ?>
+
+    <!-- The js behind the leaderboard -->
+    <script src="../Scripts/leaderboard.js"></script>
 </head>
 
 <html>
 <body>
     <?php include '../Elements/navbar.php' ?>
     <div class="viewport" id="main">
-        
+        <div class="horizontally-centered-container">
+            <button type="button" class="btn btn-primary">Round 1</button>
+            <button type="button" class="btn btn-secondary">Round 2</button>
+            <button type="button" class="btn btn-success">Round 3</button>
+            <button type="button" class="btn btn-danger">Round 4</button>
+            <button type="button" class="btn btn-warning">Round 5</button>
+            <button type="button" class="btn btn-info">Round 6</button>
+            <button type="button" class="btn btn-dark">Total Score</button>
+        </div>
+
+        <table>
+            <thead>
+                <tr id=header>
+                    <th>Avatar</th>
+                    <th>Username</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+
+            <!-- contain all other rows in the table -->
+            <tbody>
+
+            </tbody>
+        </table>
     </div>
 </body>
